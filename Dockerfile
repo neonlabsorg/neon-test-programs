6ff482ebd05aca2378fa04a5644d2f5d0ae4af1a
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-24 14:58:15 +0300

    update neon-test-invoke-program.sh

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,6 @@ RUN cargo +nightly clippy &&  cargo build-bpf
 
 FROM ubuntu:20.04
 
-WORKDIR /opt
-
-ENV PATH=/opt/solana/bin
-
 COPY --from=builder /opt/neon-test-invoke-program-keypair.json /opt
 COPY --from=builder /opt/target/deploy/neon_test_invoke_program.so /opt
 COPY --from=builder /opt/neon-test-invoke-program.sh /opt
--- neon-test-invoke-program.sh
+++ neon-test-invoke-program.sh
@@ -5,14 +5,16 @@ if [ -z "$SOLANA_URL" ]; then
   exit 1
 fi
 
-solana config set -u "$SOLANA_URL"
+SOLANA_BIN=/opt/solana/bin
 
-solana airdrop 1
+${SOLANA_BIN}/solana config set -u "$SOLANA_URL"
 
-export TEST_PROGRAM=$(solana address -k proxy_program-keypair.json)
+${SOLANA_BIN}/solana airdrop 1
+
+export TEST_PROGRAM=$(${SOLANA_BIN}/solana address -k proxy_program-keypair.json)
 
 echo "Deploying proxy_program at address $TEST_PROGRAM..."
-if ! solana program deploy --upgrade-authority neon-test-invoke-program-keypair.json neon_test_invoke_program.so ; then
+if ! ${SOLANA_BIN}/solana program deploy --upgrade-authority neon-test-invoke-program-keypair.json neon_test_invoke_program.so ; then
   echo "Failed to deploy proxy_program"
   exit 1
 fi
