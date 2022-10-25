c2efefd8ff48fd672e72a8d53ce2103f118d0e38
sinev-valentine <sinev-valentine@yandex.ru>
2022-10-25 11:45:35 +0300

    fix all

--- neon-test-invoke-program.sh
+++ neon-test-invoke-program.sh
@@ -8,13 +8,13 @@ fi
 SOLANA_BIN=/opt/solana/bin
 
 ${SOLANA_BIN}/solana config set -u "$SOLANA_URL"
-${SOLANA_BIN}/solana-keygen new --no-passphrase -o /root/.config/solana/id.json
+${SOLANA_BIN}/solana-keygen new --no-passphrase
 ${SOLANA_BIN}/solana airdrop 1
 
-export TEST_PROGRAM=$(${SOLANA_BIN}/solana address -k /opt/proxy_program-keypair.json)
+export TEST_PROGRAM=$(${SOLANA_BIN}/solana address -k /opt/neon_test_invoke_program-keypair.json)
 
 echo "Deploying proxy_program at address $TEST_PROGRAM..."
-if ! ${SOLANA_BIN}/solana program deploy --upgrade-authority /opt/neon-test-invoke-program-keypair.json /opt/neon_test_invoke_program.so ; then
+if ! ${SOLANA_BIN}/solana program deploy --upgrade-authority /opt/neon_test_invoke_program-keypair.json /opt/neon_test_invoke_program.so ; then
   echo "Failed to deploy proxy_program"
   exit 1
 fi
